85620d79fa10532dcb63d597e2bf11fb30cc00d7
laynef <laynefaler@gmail.com>
2021-12-27 09:59:52 -0800

    Added react router

--- assets/js/application.tsx
+++ assets/js/application.tsx
@@ -1,7 +1,18 @@
 import "../css/application.scss";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => <h1>Hello World!</h1>;
+const HomePage = () => <h1>Hello World!</h1>;
+const NotFoundPage = () => <h1>404: Not Found!</h1>;
+
+const App = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    </BrowserRouter>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
