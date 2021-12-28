ffe4ff841cf29e6190bcad409c2e56a369c27978
laynef <laynefaler@gmail.com>
2021-12-28 07:39:07 -0800

    Working on signup with react query

--- assets/js/application.tsx
+++ assets/js/application.tsx
@@ -8,18 +8,23 @@ import { LoginPage } from "./pages/login";
 import { SignUpPage } from "./pages/signup";
 import { DashboardPage } from "./pages/dashboard";
 import { InternalErrorPage } from "./pages/500";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+ 
+const queryClient = new QueryClient();
 
 const App = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/internal_server_error" element={<InternalErrorPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/internal_server_error" element={<InternalErrorPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </BrowserRouter>
+    </QueryClientProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- assets/js/pages/signup.tsx
+++ assets/js/pages/signup.tsx
@@ -1,31 +1,76 @@
 import * as React from 'react';
+import { useMutation } from 'react-query';
 import { Header, Layout } from '../components/layout';
 
+type RequestBody = {
+    email: string;
+    username: string;
+    password: string;
+    password_confirmation: string;
+}
+
+type ErrorResponse = {
+    message: string;
+    code: number;
+}
+
 export const SignUpPage = () => {
+    const signup = useMutation((body: RequestBody) => {
+        return fetch('/api/v1/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        })
+        .then(res => res.json())
+        .catch((err: ErrorResponse) => err)
+    }, {
+        onError: (error: ErrorResponse) => {
+            console.error(error);
+        },
+        onSuccess: (data, variables, context) => {
+            // Boom baby!
+        },
+    });
+
+    const [email, setEmail] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [password_confirmation, setPasswordConfirmation] = React.useState('');
+
     return (
         <Layout>
             <>
                 <Header />
                 <main>
                     <h1>Sign Up</h1>
-                    <form className='card' action={null}>
-                        <div className='column'>
-                            <label>Email</label>
-                            <input type='email' name='email' />
-                        </div>
+                    <form className='card' onSubmit={() => signup.mutate({
+                        email,
+                        username,
+                        password,
+                        password_confirmation,
+                    })} action={null}>
                         <div className='column'>
                             <label>Username</label>
-                            <input type='text' name='username' />
+                            <input onChange={e => setUsername(e.target.value)} type='text' name='username' />
+                        </div>
+                        <div className='column'>
+                            <label>Email</label>
+                            <input onChange={e => setEmail(e.target.value)} type='email' name='email' />
                         </div>
                         <div className='column'>
                             <label>Password</label>
-                            <input type='password' name='password' />
+                            <input onChange={e => setPassword(e.target.value)} type='password' name='password' />
                         </div>
                         <div className='column'>
                             <label>Password Confirmation</label>
-                            <input type='password' name='password_confirmation' />
+                            <input onChange={e => setPasswordConfirmation(e.target.value)} type='password' name='password_confirmation' />
                         </div>
                         <input type='submit' value='Sign Up' />
+                        {signup.isError ? (
+                            <div>An error occurred: {signup.error.message}</div>
+                        ) : null}
                     </form>
                 </main>
             </>
