414b06603ad15ab46121348d7c5209c470d84a9a
laynef <laynefaler@gmail.com>
2021-12-28 07:13:56 -0800

    Created basic forms

--- assets/js/pages/login.tsx
+++ assets/js/pages/login.tsx
@@ -8,6 +8,17 @@ export const LoginPage = () => {
                 <Header />
                 <main>
                     <h1>Login</h1>
+                    <form className='card' action={null}>
+                        <div className='column'>
+                            <label>Email</label>
+                            <input type='email' name='email' />
+                        </div>
+                        <div className='column'>
+                            <label>Password</label>
+                            <input type='password' name='password' />
+                        </div>
+                        <input type='submit' value='Login' />
+                    </form>
                 </main>
             </>
         </Layout>
--- assets/js/pages/signup.tsx
+++ assets/js/pages/signup.tsx
@@ -8,6 +8,25 @@ export const SignUpPage = () => {
                 <Header />
                 <main>
                     <h1>Sign Up</h1>
+                    <form className='card' action={null}>
+                        <div className='column'>
+                            <label>Email</label>
+                            <input type='email' name='email' />
+                        </div>
+                        <div className='column'>
+                            <label>Username</label>
+                            <input type='text' name='username' />
+                        </div>
+                        <div className='column'>
+                            <label>Password</label>
+                            <input type='password' name='password' />
+                        </div>
+                        <div className='column'>
+                            <label>Password Confirmation</label>
+                            <input type='password' name='password_confirmation' />
+                        </div>
+                        <input type='submit' value='Sign Up' />
+                    </form>
                 </main>
             </>
         </Layout>
